00eb950cfe0ca948bed96bc493a17c7815e1b960
prestontravis <119766277+prestontravis@users.noreply.github.com>
2023-06-09 18:57:35 -0400

    Heroku push

--- server.js
+++ server.js
@@ -1,20 +1,44 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
-const server = express();
-const port = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-server.use(express.urlencoded({ extended: true }));
-server.use(express.json());
-server.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
-// Routes
-const indexRoutes = require('./routes/index');
-const notesRoutes = require('./routes/notes');
-server.use('/', indexRoutes);
-server.use('/notes', notesRoutes);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-// Start the server
-server.listen(port, () => {
-  console.log(`App listening on PORT ${port}`);
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db', 'db.json'), 'utf8'));
+  res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db', 'db.json'), 'utf8'));
+  const newNote = req.body;
+  newNote.id = Date.now().toString();
+  notes.push(newNote);
+  fs.writeFileSync(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes));
+  res.json(newNote);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db', 'db.json'), 'utf8'));
+  const noteId = req.params.id;
+  const updatedNotes = notes.filter((note) => note.id !== noteId);
+  fs.writeFileSync(path.join(__dirname, 'db', 'db.json'), JSON.stringify(updatedNotes));
+  res.json({ success: true });
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT ${PORT}`);
 });
